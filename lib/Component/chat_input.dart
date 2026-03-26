58cd81e047dcc6b37f801f13fb7a0f5a533b27c7
MinhDCAI <daochiminh@cybersoft.com.vn>
2026-03-26 11:23:28 +0700

    u

--- lib/Component/chat_input.dart
+++ lib/Component/chat_input.dart
@@ -121,6 +121,7 @@ class _ChatInputState extends State<ChatInput> {
     );
 
     _textController.clear();
+    _focusNode.unfocus();
     setState(() {
       _state = _state.copyWith(isEditing: false);
     });
